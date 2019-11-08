0e0ba66de9f2087508fef200f37f8f4599785435
SinghDigamber <digambersingh126@gmail.com>
2019-11-09 03:17:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ContactComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { variable } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+
+export class ProductDetailComponent {
+  product_id: string;
+
+  constructor(private actRoute: ActivatedRoute) {
+    this.product_id = this.actRoute.snapshot.params.id;
+  }
+}
